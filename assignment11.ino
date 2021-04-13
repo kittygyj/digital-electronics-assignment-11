ed1fc54451ccdcb5fa9d14dcfc22fc6f0917978c
kittygyj <82459117+kittygyj@users.noreply.github.com>
2021-04-13 22:54:25 +0800

    Update assignment11.ino
    Using for loops to simplify the codes

--- assignment11.ino
+++ assignment11.ino
@@ -5,33 +5,21 @@
   This example code is in the public domain.
  */
 
-int ledOne = 2;
-int ledTwo = 4;
-int ledThree = 6;
-int ledFour = 8;
+int ledPins[4] = {2, 4, 6, 8};
 
 void setup() {
-  pinMode(ledOne, OUTPUT);
-  pinMode(ledTwo, OUTPUT);
-  pinMode(ledThree, OUTPUT);
-  pinMode(ledFour, OUTPUT);
+  for (i = 0; i < 4; i++) {
+    pinMode(ledPins[i], OUTPUT);
+  }
 }
 
 void loop() {
-  digitalWrite(ledOne, HIGH);
-  delay(1000);             
-  digitalWrite(ledTwo, HIGH);
-  delay(1000);
-  digitalWrite(ledThree, HIGH);
-  delay(1000);             
-  digitalWrite(ledFour, HIGH);
-  delay(1000);
-  digitalWrite(ledFour, LOW);
-  delay(1000);
-  digitalWrite(ledThree, LOW);
-  delay(1000);
-  digitalWrite(ledTwo, LOW);
-  delay(1000);
-  digitalWrite(ledOne, LOW);
-  delay(1000);
+  for (i = 0; i < 4; i++) {
+    digitalWrite(ledPins[i], HIGH);
+    delay(1000);
+  }
+  for (i = 3; i > -1; i--) {
+    digitalWrite(ledPins[i], LOW);
+    delay(1000);
+  }
 }
